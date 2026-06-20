df70828377a463b1c4f2905c57e9b11b3ff9214d
ℒ <cosmiclegendstudio@gmail.com>
2026-06-20 08:55:28 +0545

    Add files via upload

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,42 +1,36 @@
 package types
 
-import (
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/gotd/td/session"
-)
+import "time"
 
-type Part struct {
-	DecryptedSize int64
-	Size          int64
-	Salt          string
-	ID            int64
+type File struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Size      int64     `json:"size"`
+	MimeType  string    `json:"mime_type"`
+	ParentID  *string   `json:"parent_id,omitempty"`
+	ChannelID int64     `json:"channel_id"`
+	MessageID int       `json:"message_id"`
+	Parts     int       `json:"parts"`
+	Encrypted bool      `json:"encrypted"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type JWTClaims struct {
-	jwt.RegisteredClaims
-	Name      string `json:"name"`
-	UserName  string `json:"userName"`
-	IsPremium bool   `json:"isPremium"`
-	Hash      string `json:"hash"`
-	TgSession string `json:"tgSession,omitempty"`
+type User struct {
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Phone     string    `json:"phone"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
-type SessionData struct {
-	Version int
-	Data    session.Data
-}
-type SocketMessage struct {
-	AuthType      string `json:"authType"`
-	Message       string `json:"message"`
-	PhoneNo       string `json:"phoneNo,omitempty"`
-	PhoneCodeHash string `json:"phoneCodeHash,omitempty"`
-	PhoneCode     string `json:"phoneCode,omitempty"`
-	Password      string `json:"password,omitempty"`
-}
-
-type BotInfo struct {
-	Id         int64
-	UserName   string
-	AccessHash int64
-	Token      string
+type UploadSession struct {
+	ID        string    `json:"id"`
+	UserID    int64     `json:"user_id"`
+	FileName  string    `json:"file_name"`
+	FileSize  int64     `json:"file_size"`
+	ChunkSize int64     `json:"chunk_size"`
+	Chunks    int       `json:"chunks"`
+	Uploaded  int       `json:"uploaded"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
 }
